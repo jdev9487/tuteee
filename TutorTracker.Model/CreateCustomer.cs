65f8db509752269dc56c23a4be3318db1963d3c0
John Gould <johngould9487@protonmail.com>
2025-07-14 17:54:53 +0100

    removed old ui and api projects

--- TutorTracker.Model/CreateCustomer.cs
+++
@@ -1,9 +0,0 @@
-namespace TutorTracker.Model;
-
-public class CreateCustomer
-{
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-    public string Email { get; set; } = default!;
-    public string Phone { get; set; } = default!;
-}
