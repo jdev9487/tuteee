43cf4847dd5b236934eb6742828a772bc08a75bd
John Gould <johngould9487@protonmail.com>
2025-07-20 18:00:02 +0100

    rework

--- ui/src/app/types/GuardianResponse.ts
+++
@@ -1,9 +0,0 @@
-import { tuteeResponse } from "./TuteeResponse"
-
-export type accountResponse = {
-    accountId: number, 
-    holderFirstName: string,
-    holderLastName: string,
-    tutees: tuteeResponse[]
-}
-
