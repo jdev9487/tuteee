e47da4e3ccebb5f952926f239b4d135b29ef5214
John Gould <johngould9487@protonmail.com>
2025-07-20 11:12:09 +0100

    migration testing

---
+++ api/JDev.Tuteee.Api/Migrations/20250720100457_LastFourToAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JDev.Tuteee.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastFourToAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastFour",
+                table: "Account",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastFour",
+                table: "Account");
+        }
+    }
+}
