28fc4492f9d7222e5e61cde59b453ef855238fe2
John Gould <johngould9487@protonmail.com>
2025-10-24 20:57:11 +0100

    quotes

--- JDev.Tuteee.Grpc.Api/bao-initialise.sh
+++ JDev.Tuteee.Grpc.Api/bao-initialise.sh
@@ -4,11 +4,11 @@ postgresToken=$(bao write auth/approle/login -format=json \
     role_id="$(cat /run/secrets/grpc_role_id)" \
     secret_id="$(cat /run/secrets/grpc_secret_id)" | jq '.auth.client_token')
 
-bao login -no-print "$postgresToken"
+bao login -no-print $postgresToken
 
 postgresLogin=$(bao read database/creds/jdev-tuteee-grpc -format=json)
-postgresUsername=$(echo "$postgresLogin" | jq '.data.username' )
-postgresPassword=$(echo "$postgresLogin" | jq '.data.password' )
+postgresUsername=$(echo $postgresLogin | jq '.data.username' )
+postgresPassword=$(echo $postgresLogin | jq '.data.password' )
 
 echo "{ \"ConnectionStrings\": { \"Tuteee\": \"Host=a369850-akamai-prod-6636035-default.g2a.akamaidb.net;Port=23590;Username=$postgresUsername;Password=$postgresPassword;Database=JDev.Tuteee.Rest;Sslmode=require\" } }" | jq "." >> appsettings.Secret.json
 
