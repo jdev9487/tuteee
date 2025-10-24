ceff68fca8fd3810c2337b90aa34cade3bc5f230
John Gould <johngould9487@protonmail.com>
2025-10-24 15:21:33 +0100

    grpc

--- JDev.Tuteee.Grpc.Api/bao-initialise.sh
+++ JDev.Tuteee.Grpc.Api/bao-initialise.sh
@@ -1,16 +1,14 @@
 export BAO_ADDR=https://infra.bao.johngould.net:3001
 
-postgresToken=$(bao write auth/approle/login \
-    role_id="$(cat /run/secrets/postgres_grpc_role_id)" \
-    secret_id="$(cat /run/secrets/postgres_grpc_secret_id)" | grep -E '\btoken\b' | sed 's/^.* //')
+postgresToken=$(bao write auth/approle/login -format=json \
+    role_id="$(cat /run/secrets/grpc_role_id)" \
+    secret_id="$(cat /run/secrets/grpc_secret_id)" | jq '.auth.client_token')
 
 bao login -no-print "$postgresToken"
 
-bao read database/creds/jdev-tuteee-rest > creds
-postgresUsername=$(cat creds | grep -E '\busername\b' | sed 's/^.* //')
-postgresPassword=$(cat creds | grep -E '\bpassword\b' | sed 's/^.* //')
-
-rm creds
+postgresLogin=$(bao read database/creds/jdev-tuteee-grpc -format=json)
+postgresUsername=$(echo "$postgresLogin" | jq '.data.username' )
+postgresPassword=$(echo "$postgresLogin" | jq '.data.password' )
 
 echo "{ \"ConnectionStrings\": { \"Tuteee\": \"Host=a369850-akamai-prod-6636035-default.g2a.akamaidb.net;Port=23590;Username=$postgresUsername;Password=$postgresPassword;Database=JDev.Tuteee.Rest;Sslmode=require\" } }" | jq "." >> appsettings.Secret.json
 
--- JDev.Tuteee.Grpc.Api/initialise.sh
+++
@@ -1,7 +0,0 @@
-rabbit_username=$(cat /app/rabbitmq/username)
-rabbit_password=$(cat /app/rabbitmq/password)
-
-export RABBIT_MQ_USERNAME="$rabbit_username"
-export RABBIT_MQ_PASSWORD="$rabbit_password"
-
-dotnet JDev.Tuteee.Grpc.Api.dll
