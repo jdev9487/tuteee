b477befe79a8cb4822a509c069faffaed20fcae1
John Gould <johngould9487@protonmail.com>
2025-10-29 15:46:14 +0000

    push

--- EmailConsumer.Dockerfile
+++ EmailConsumer.Dockerfile
@@ -15,8 +15,8 @@ RUN dotnet build --no-restore --configuration Release ./JDev.Tuteee.EmailConsume
 
 RUN dotnet publish --no-build -o out ./JDev.Tuteee.EmailConsumer/JDev.Tuteee.EmailConsumer.csproj
 
-FROM mcr.microsoft.com/dotnet/aspnet:9.0
+FROM infra.registry.johngould.net/bao-dotnet:0.1.3
+
 WORKDIR /app
-COPY --from=openbao/openbao:2.4.1 /bin/bao /bin
+
 COPY --from=build /app/out .
-COPY JDev.Tuteee.EmailConsumer/initialise.sh /app/initialise.sh
--- JDev.Tuteee.EmailConsumer/Program.cs
+++ JDev.Tuteee.EmailConsumer/Program.cs
@@ -2,26 +2,17 @@ using JDev.Core.RabbitMQ;
 using JDev.Tuteee.EmailConsumer;
 using JDev.Tuteee.EmailConsumer.Auth;
 using JDev.Tuteee.EmailConsumer.Clients;
-using MailKit.Security;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var auth = new Auth
-{
-    Username = Environment.GetEnvironmentVariable("RABBIT_MQ_USERNAME") ?? "",
-    Password = Environment.GetEnvironmentVariable("RABBIT_MQ_PASSWORD") ?? ""
-};
+builder.Configuration.AddJsonFile("appsettings.Secret.json", optional: true);
+
+var auth = new Auth();
 builder.Configuration.GetSection("RabbitMQ").Bind(auth);
 
 builder.Services.AddRabbitMqConsumer<EmailHomeworkHandler>(auth);
 
 builder.Services.AddOptions<Email>()
-    .Configure(x =>
-    {
-        x.Username = Environment.GetEnvironmentVariable("PROTONMAIL_USERNAME") ?? "";
-        x.Password = Environment.GetEnvironmentVariable("PROTONMAIL_PASSWORD") ?? "";
-    })
     .BindConfiguration("Email");
 
 builder.Services.AddTransient<IEmailClient, EmailClient>();
