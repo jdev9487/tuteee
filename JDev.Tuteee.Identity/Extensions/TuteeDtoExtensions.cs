de230c7a8f17a126af2b8b6797c309db0c5a9ae3
John Gould <johngould9487@protonmail.com>
2025-11-25 08:02:40 +0000

    cards instead of table

--- JDev.Tuteee.Identity/Extensions/TuteeDtoExtensions.cs
+++ JDev.Tuteee.Identity/Extensions/TuteeDtoExtensions.cs
@@ -4,7 +4,7 @@ using Rest.ApiClient.DTOs;
 
 public static class TuteeDtoExtensions
 {
-     private static bool IsSelfPaying(this TuteeDto tuteeDto) => tuteeDto.StakeholderId == tuteeDto.Client?.StakeholderId;
+     public static bool IsSelfPaying(this TuteeDto tuteeDto) => tuteeDto.StakeholderId == tuteeDto.Client?.StakeholderId;
      public static string GetFirstName(this TuteeDto tuteeDto) => tuteeDto.IsSelfPaying() ? "SELF" : tuteeDto.FirstName;
      public static string GetLastName(this TuteeDto tuteeDto) => tuteeDto.IsSelfPaying() ? "" : tuteeDto.LastName;
      public static string ToActiveRateCurrency(this TuteeDto tuteeDto) => (tuteeDto.GetActiveRate() / 100).ToString("0.00");
